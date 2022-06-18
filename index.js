dc95a3c730b72b81d82386afd7f71cc31d9decf0
mbolonzi-design <mbolonzi.design@gmail.com>
2022-06-18 12:11:45 +0300

    commit tree 45a35bfb2dd249cc5ebf257e6302aa3af770472f

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = word.split('').reverse().join('')
+
+  return word === reversedWord;
 }
 
 /* 
   Add your pseudocode here
+   Create a new variable reversedWord
+  Split it, reverse the splitted word and joining it back to a string
+  If the reversed word is same as the original word it returns true else false
 */
 
 /*
   Add written explanation of your solution here
+    I created a new variable reversedWord which is equivalent to the word which is splitted, reversed and joined
+  The code checks if the reverseWord is equal to word and it returns true else false if they are not equal
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("peep"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("cat"));
+  
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
